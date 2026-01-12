18fe23aeb98919721e2cba6b47fbef09db2d7621
Kaszás Szófia <szofia.kaszas@gmail.com>
2026-01-12 18:14:41 +0100

    added comments

--- src/house-listing/house-listing.service.ts
+++ src/house-listing/house-listing.service.ts
@@ -6,22 +6,26 @@ import { PrismaService } from 'src/prisma.service';
 @Injectable()
 export class HouseListingService {
   constructor(private readonly db: PrismaService) {}
+  // create a new house listing
   create(createHouseListingDto: CreateHouseListingDto) {
     return this.db.houseListing.create({
       data: createHouseListingDto,
     });
   }
 
+  // get all house listings
   findAll() {
     return this.db.houseListing.findMany();
   }
 
+  // get a single house listing by id
   findOne(id: number) {
     return this.db.houseListing.findUnique({
       where: { idHouse: id },
     });
   }
 
+  // update a house listing by id
   update(id: number, updateHouseListingDto: UpdateHouseListingDto) {
     return this.db.houseListing.update({
       where: { idHouse: id },
@@ -29,6 +33,7 @@ export class HouseListingService {
     });
   }
 
+  // delete a house listing by id
   remove(id: number) {
     return this.db.houseListing.delete({
       where: { idHouse: id },
