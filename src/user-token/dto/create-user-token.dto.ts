fe136c1709441c05db2ffc1bf7e88980746189ff
Petrik Diak <diak@petrik.hu>
2026-02-12 10:03:50 +0100

    user-token documentation done

--- src/user-token/dto/create-user-token.dto.ts
+++ src/user-token/dto/create-user-token.dto.ts
@@ -1,21 +1,16 @@
-import { IsNotEmpty, IsNumber, /*IsPositive,*/ IsString } from "class-validator"
+import { ApiProperty } from "@nestjs/swagger"
+import { IsNotEmpty, IsNumber, IsString } from "class-validator"
 
 /**
  * Dto for creating a new record
  */
 export class CreateUserTokenDto {
-  /**
-   * Id od the user who's token this is
-   * @example 42
-   */
+  @ApiProperty({description: "Id of the user who's token this is", example: 42})
   @IsNotEmpty()
   @IsNumber()
   userIdToken: number
 
-  /**
-   * The token assigned to the user
-   * @example f8bf1a217dde79dad1219f11345ca5ef792d341e556b7c1cf11576274c9a9062
-   */
+  @ApiProperty({description: 'The token assigned to the user', example: 'f8bf1a217dde79dad1219f11345ca5ef792d341e556b7c1cf11576274c9a9062'})
   @IsNotEmpty()
   @IsString()
   token: string
--- src/user-token/user-token.controller.ts
+++ src/user-token/user-token.controller.ts
@@ -13,15 +13,24 @@ import { UserTokenService } from './user-token.service';
 import { UpdateUserTokenDto } from './dto/update-user-token.dto';
 import { UserService } from 'src/user/user.service';
 import { LoginDto } from './dto/login.dto';
-import { ApiForbiddenResponse } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiExtraModels,
+  ApiForbiddenResponse,
+  ApiOkResponse,
+  ApiParam,
+} from '@nestjs/swagger';
+import { CreateUserTokenDto } from './dto/create-user-token.dto';
 
 /**
  * Controller for managing user tokens
- * 
+ *
  * Handles:
  * -Login
  * -CRUD for user tokens
  */
+@ApiExtraModels(CreateUserTokenDto)
 @Controller('user-token')
 export class UserTokenController {
   constructor(
@@ -31,11 +40,23 @@ export class UserTokenController {
 
   /**
    * Login for a user using email and password
-   * 
+   *
    * @param loginDto login data (email, password)
    * @returns an object containing the token that's just been created
    */
-  @ApiForbiddenResponse({ description: 'Invalid email or password'})
+  @ApiBody({
+    description: 'Login credentials',
+    schema: {
+      example: {
+        email: 'user@example.com',
+        password: 'strongPassword123',
+      },
+    },
+  })
+  @ApiCreatedResponse({
+    description: 'Successfully logged in. Returns a newly created token.',
+  })
+  @ApiForbiddenResponse({ description: 'Invalid email or password' })
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
     const user = await this.userService.findByEmail(loginDto.email);
@@ -52,11 +73,12 @@ export class UserTokenController {
 
   /**
    * Gets all user tokens
-   * 
+   *
    * @returns an array of all user token records
-   * 
+   *
    * TODO: authenticate so only admin user can do this
    */
+  @ApiOkResponse({ description: 'Ok. Returns all tokens' })
   @Get()
   findAll() {
     return this.userTokenService.findAll();
@@ -64,10 +86,12 @@ export class UserTokenController {
 
   /**
    * Gets one token by its id (not sure if needed)
-   * 
+   *
    * @param id token id
    * @returns the token thats the users
    */
+  @ApiParam({ name: 'id', description: 'id of the token updated', example: 25 })
+  @ApiOkResponse({ description: 'Ok. Returns one token by its id' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userTokenService.findOne(+id);
@@ -75,11 +99,34 @@ export class UserTokenController {
 
   /**
    * Updates a tokens data in the table by the tokens id
-   * 
+   *
    * @param id token id
    * @param updateUserTokenDto partial update data
    * @returns the updated user token record
    */
+  @ApiBody({
+    description: 'Fields to update for the user token',
+    schema: {
+      example: {
+        userIdToken: 42,
+        token:
+          'f8bf1a217dde79dad1219f11345ca5ef792d341e556b7c1cf11576274c9a9062',
+      },
+    },
+  })
+  @ApiParam({ name: 'id', description: 'id of the token updated', example: 42 })
+  @ApiBody({
+    description: 'fields to update',
+    schema: {
+      example: {
+        token: 'token',
+      },
+    },
+  })
+  @ApiOkResponse({
+    description: 'Ok. Updating token successful',
+    type: CreateUserTokenDto,
+  })
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -90,10 +137,15 @@ export class UserTokenController {
 
   /**
    * Deletes user token by token id
-   * 
+   *
    * @param id token id
    * @returns the deleted user token record
    */
+  @ApiParam({ name: 'id', description: 'id of the token updated', example: 42 })
+  @ApiOkResponse({
+    description: 'Ok. Token deleted successfully',
+    type: CreateUserTokenDto,
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userTokenService.remove(+id);
