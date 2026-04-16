8b876d65e958ec6596d477aa536dbc9aa146b698
Peti39 <szpeti2006@gmail.com>
2026-04-16 04:19:01 +0200

    finished ratings

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -7,75 +7,105 @@ import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class AdminService {
-  constructor(private readonly db:PrismaService){}
+  constructor(private readonly db: PrismaService) {}
+
   async aproveHouseRating(ratingId: number) {
     try {
-      return await this.db.houseListingRatings.update({
-        where:{id: ratingId},
-        data:{approved:true}
+      const rating = await this.db.houseListingRatings.update({
+        where: { id: ratingId },
+        data: { approved: true },
+      });
+      const allRatings = await this.db.houseListingRatings.findMany({
+        where: {
+          ratedHouseId: rating.ratedHouseId,
+          approved: true,
+        },
+      });
+      const totalRating = allRatings.reduce((total, rating) => {
+        return total + rating.ratingScore;
+      }, 0);
+
+      const averageRating = allRatings.length
+        ? totalRating / allRatings.length
+        : 0;
+
+      await this.db.houseListing.update({
+        where:{idHouse: rating.ratedHouseId},
+        data: {rating: averageRating}
       })
+      return rating;
     } catch (error) {
-      handlePrismaError(error)
-    };
+      handlePrismaError(error);
+    }
   }
   async aproveRoommateRating(ratingId: number) {
     try {
-      return await this.db.roommateRatings.update({
-        where:{id: ratingId},
-        data:{approved:true}
+      const rating = await this.db.roommateRatings.update({
+        where: { id: ratingId },
+        data: { approved: true },
+      });
+      const allRatings = await this.db.roommateRatings.findMany({
+        where:{
+          ratedUserId: rating.ratedUserId,
+          approved: true
+        }
       })
+      const totalRating = allRatings.reduce((total, rating) => {return total + rating.ratingScore},0)
+      const averageRating = allRatings.length ? totalRating / allRatings.length : 0;
+      await this.db.user.update({
+        where:{idUser: rating.ratedUserId},
+        data:{rating: averageRating}
+      })
+      return rating
     } catch (error) {
-      handlePrismaError(error)
+      handlePrismaError(error);
     }
   }
   async allUnaprovedHouseRatings() {
     try {
       return await this.db.houseListingRatings.findMany({
-        where:{approved: false}
-      })
+        where: { approved: false },
+      });
     } catch (error) {
-      handlePrismaError(error)
+      handlePrismaError(error);
     }
   }
   async allUnaprovedRoommateRatings() {
     try {
       return await this.db.roommateRatings.findMany({
-        where:{approved: false}
-      })
+        where: { approved: false },
+      });
     } catch (error) {
-      handlePrismaError(error)
+      handlePrismaError(error);
     }
   }
   async adminList() {
     try {
       return await this.db.user.findMany({
-        where:{role:"admin"}
-      })
+        where: { role: 'admin' },
+      });
     } catch (error) {
-      handlePrismaError(error)
+      handlePrismaError(error);
     }
   }
-  
+
   async create(createAdminDto: CreateAdminDto) {
     try {
       const newAdmin = {
         ...createAdminDto,
-        role:'admin',
-        password: await argon2.hash(createAdminDto.password)
-      }
+        role: 'admin',
+        password: await argon2.hash(createAdminDto.password),
+      };
       return await this.db.user.create({
-        data:{
-          ...newAdmin
+        data: {
+          ...newAdmin,
         },
-        omit:{
-          password: true
-        }
-      })
+        omit: {
+          password: true,
+        },
+      });
     } catch (error) {
-      handlePrismaError(error)
+      handlePrismaError(error);
     }
-     
   }
-
-  
 }
