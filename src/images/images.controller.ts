ef0ad01edc228af80c2881ef6848a58273e10afd
Hunyorító <fecsohunor@gmail.com>
2026-04-21 22:33:29 +0200

    feat(images): integrate AWS S3 for image uploads and management
    - Added AWS SDK S3 client to handle image uploads.
    - Implemented image upload functionality in ImagesController and ImagesService.
    - Added methods to retrieve images and count, set profile images, and delete images.
    - Updated Prisma schema to include new fields for user images.
    - Added dependency for @aws-sdk/client-s3 and @types/multer.

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,34 +1,47 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { BadRequestException,UseInterceptors, UploadedFile,Controller, Get, Post, Body, Patch, Param,ParseIntPipe, Delete } from '@nestjs/common';
 import { ImagesService } from './images.service';
 import { CreateImageDto } from './dto/create-image.dto';
 import { UpdateImageDto } from './dto/update-image.dto';
+import { FileInterceptor } from "@nestjs/platform-express";
 
 @Controller('images')
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) {}
 
-  @Post()
-  create(@Body() createImageDto: CreateImageDto) {
-    return this.imagesService.create(createImageDto);
+  @Post("upload")
+  @UseInterceptors(FileInterceptor("file", { limits: { fileSize: 2 * 1024 * 1024 } }))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    const userIdImages=1
+    if(!file){
+      throw new BadRequestException("No file uploaded")
+    }
+    if(!file.mimetype.startsWith("image/")){
+      throw new BadRequestException("Only pictures are allowed")
+    }
+    return this.imagesService.upload(file,userIdImages);
   }
 
-  @Get()
-  findAll() {
-    return this.imagesService.findAll();
+  @Get("images")
+  getImages(){
+    const userIdImages=1
+    return this.imagesService.getImages(userIdImages)
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.imagesService.findOne(+id);
+  @Get("image-count")
+  getImageCount(){
+    const userIdImages=1
+    return this.imagesService.getImageCount(userIdImages)
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateImageDto: UpdateImageDto) {
-    return this.imagesService.update(+id, updateImageDto);
+  @Post("profile/:id")
+  setProfile(@Param("id",ParseIntPipe) id:number){
+    const userIdImages=1
+    return this.imagesService.setProfile(userIdImages,id)
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.imagesService.remove(+id);
+  @Delete(":id")
+  deletImag(@Param("id",ParseIntPipe) id:number){
+    const userIdImages=1
+    return this.imagesService.deletImag(id,userIdImages)
   }
 }
--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -1,28 +1,108 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateImageDto } from './dto/create-image.dto';
 import { UpdateImageDto } from './dto/update-image.dto';
 import { PrismaService } from 'src/prisma.service';
+import { s3 } from "src/s3/s3.service";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { Express } from "express";
+import { Multer } from 'multer';
 
 @Injectable()
 export class ImagesService {
   constructor(private readonly db:PrismaService) {}
-  create(createImageDto: CreateImageDto) {
-    return 'This action adds a new image';
+  private bucket = "test-image";
+  //private Max_Imag=5;
+
+  async getImages(userIdImages:number){
+    //console.log("teszt get images")
+    const images=await this.db.userImages.findMany({
+      where:{
+        userIdImages,
+        deleted: false
+      }
+      
+    })
+    const val=images.map(img=>({
+      id:img.id,
+      IsProfile:img.IsProfile,
+      url:`http://localhost:9000/${this.bucket}/${img.key}`
+
+    }))
+    //console.log(val)
+    return images.map(img=>({
+      id:img.id,
+      IsProfile:img.IsProfile,
+      url:`http://localhost:9000/${this.bucket}/${img.key}`
+
+    }))
+  }
+  async getImageCount(userIdImages:number){
+    const count=await this.db.userImages.count({
+      where:{
+        userIdImages,
+        deleted: false
+      }
+    })
+    return count
+  }
+  async setProfile(userIdImages:number, id:number){
+    await this.db.userImages.updateMany({
+      where:{userIdImages},
+      data:{IsProfile:false}
+    });
+    return this.db.userImages.update({
+      where: {id:id},
+      data:{IsProfile:true},
+    })
+  }
+  async upload(file: Express.Multer.File, userIdImages:number) {
+    const Max_Imag=5
+   
+    const count=await this.db.userImages.count({
+      where:{
+        userIdImages,
+        deleted: false
+      }
+    })
+    if(count>=Max_Imag){
+      throw new BadRequestException("Reached maximum image limit")
+
+    }
+  const safeName = file.originalname.replace(/[^a-zA-Z0-9.\-_]/g, "_");
+  const key = `${Date.now()}-${safeName}`;
+  const url = `http://localhost:9000/${this.bucket}/${key}`;
+
+    await s3.send(
+      new PutObjectCommand({
+        Bucket: this.bucket,
+        Key: key,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+      })
+    );
+
+    const image=await this.db.userImages.create({
+      data:{
+        userIdImages,
+        key,
+        url,
+        IsProfile: false,
+        
+      }
+    })
+
+    return {
+      message: "Upload successful",
+      id:image.id,
+      url: url,
+    };
+  }
+  async deletImag(id:number,userIdImages:number){
+    
+    return await this.db.userImages.update({
+      where:{id:id},
+      data:{deleted:true}
+    })
   }
 
-  findAll() {
-    return `This action returns all images`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} image`;
-  }
-
-  update(id: number, updateImageDto: UpdateImageDto) {
-    return `This action updates a #${id} image`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} image`;
-  }
 }
