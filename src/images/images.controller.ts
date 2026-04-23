bf7d05fa60c13507ac35ecc5b28c516717b1912c
Hunyorító <fecsohunor@gmail.com>
2026-04-23 09:47:15 +0200

    feat(images): enhance house image management with upload, retrieval, and deletion functionalities

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -8,6 +8,7 @@ import { User } from 'generated/prisma/client';
 import { AuthGuard } from '@nestjs/passport';
 import { isAuthorized } from 'src/helperFunctions/helpers';
 import {type Express } from 'express';
+import { hu } from '@faker-js/faker';
 
 @Controller('images')
 export class ImagesController {
@@ -31,19 +32,53 @@ export class ImagesController {
     }
     return this.imagesService.upload(file,user.idUser);
   }
+  @Post("upload-house/:houseId")
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('bearer'))
+  @UseInterceptors(FileInterceptor("file", { limits: { fileSize: 2 * 1024 * 1024 } }))
+  uploadHouseImage(
+    @UploadedFile() file: Express.Multer.File,
+    @Request() request,
+    @Param("houseId",ParseIntPipe) houseId:number
+  ) {
+    console.log("upload endpoint called")
+
+    const user = request.user as User
+    if(!file){
+      throw new BadRequestException("No file uploaded")
+    }
+    if(!file.mimetype.startsWith("image/")){
+      throw new BadRequestException("Only pictures are allowed")
+    }
+    return this.imagesService.uploadHouseImage(file, user.idUser, houseId);
+  }
+    
   
-  @Get("image-count/:id")
+  @Get("image-user-count/:id")
   getImageCount(
     @Param("id", ParseIntPipe) id:number
   ){
     return this.imagesService.getImageCount(id)
   }
+  @Get("image-house-count/:id")
+  getHouseImageCount(
+    @Param("id", ParseIntPipe) id:number
+  ){
+    return this.imagesService.getHouseImageCount(id)
+  }
 
-  @Get("images/:id")
+
+  @Get("user-images/:id")
   getImages(
     @Param("id", ParseIntPipe) id:number
   ){
     return this.imagesService.getImages(id)
+  }
+  @Get("house-images/:id")
+  getHouseImages(
+    @Param("id", ParseIntPipe) id:number
+  ){
+    return this.imagesService.getHouseImages(id)
   }
 
 
@@ -57,6 +92,18 @@ export class ImagesController {
     const user = request.user as User
     return this.imagesService.setProfile(user.idUser,imageId)
   }
+  @Post("profile-house/:houseId/:imageId")
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('bearer'))
+  setProfileHouse(
+    @Param("imageId",ParseIntPipe) imageId:number,
+    @Param("houseId",ParseIntPipe) houseId:number,
+    @Request() request  
+  ){
+    const user = request.user as User
+    return this.imagesService.setProfileHouse(user.idUser,houseId,imageId)
+  }
+
 
   @Delete(":imageId")
   @ApiBearerAuth()
@@ -67,5 +114,15 @@ export class ImagesController {
   ){
     const user = request.user as User
     return this.imagesService.deletImag(imageId,user)
+  }
+  @Delete(":houseImageId")
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('bearer'))
+  deletHouseImage(
+    @Param("houseImageId",ParseIntPipe) houseImageId:number,
+    @Request() request
+  ){
+    const user = request.user as User
+    return this.imagesService.deletHouseImage(houseImageId,user)
   }
 }
--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -14,6 +14,7 @@ import { Express } from 'express';
 //import { Multer } from 'multer';
 import { User } from 'generated/prisma/client';
 import { handlePrismaError, isAuthorized } from 'src/helperFunctions/helpers';
+import { hu } from '@faker-js/faker';
 
 @Injectable()
 export class ImagesService {
@@ -134,4 +135,111 @@ export class ImagesService {
       handlePrismaError(error);
     }
   }
+  async uploadHouseImage(file: Express.Multer.File, userId: number, houseId: number) {
+    const Max_Imag = 8;
+    try {
+      const count = await this.db.houseImages.count({
+        where: {
+          houseIdImages: houseId,
+          deleted: false,
+        },
+      });
+      if (count >= Max_Imag) {
+        throw new BadRequestException('Reached maximum image limit');
+      }
+      const safeName = file.originalname.replace(/[^a-zA-Z0-9.\-_]/g, '_');
+      const key = `${Date.now()}-${safeName}`;
+      const url = `http://localhost:9000/${this.bucket}/${key}`;
+
+      console.log("s3 called")
+      const resp = await s3.send(
+        new PutObjectCommand({
+          Bucket: this.bucket,
+          Key: key,
+          Body: file.buffer,
+          ContentType: file.mimetype,
+        }),
+      );
+      console.log("s3 done")
+      
+      if(resp){console.log(resp)}
+
+      const image = await this.db.houseImages.create({
+        data: {
+          houseIdImages: houseId,
+          key,
+          url,
+          IsProfile: false,
+        },
+      });
+
+      return image;
+    } catch (error) {
+      handlePrismaError(error);
+      throw new InternalServerErrorException("s3 has issues (=^ ◡ ^=) ") 
+    }
+  }
+  async getHouseImageCount(houseId: number) {
+    try {
+      const count = await this.db.houseImages.count({
+        where: {
+          houseIdImages: houseId,
+          deleted: false,
+        },
+      });
+      return count;
+    } catch (error) {
+      handlePrismaError(error);
+    }
+  }
+  async getHouseImages(houseId: number) {
+    try {
+      const images = await this.db.houseImages.findMany({
+        where: {
+          houseIdImages: houseId,
+          deleted: false,
+        },
+      });
+      return images;
+    } catch (error) {
+      handlePrismaError(error);
+    }
+
+  }
+  async setProfileHouse(userId: number, houseId: number, imageId: number) {
+    try {
+      await this.db.houseImages.updateMany({
+        where: { houseIdImages: houseId },
+        data: { IsProfile: false },
+      });
+      return await this.db.houseImages.update({
+        where: { idHouseImage: imageId},
+        data: { IsProfile: true },
+      });
+    } catch (error) {
+      handlePrismaError(error);
+    }
+  }
+
+  async deletHouseImage( houseImageId: number, user: User) {
+    try {
+      const image = await this.db.houseImages.findUnique({
+        where: { idHouseImage: houseImageId },
+      });
+      if (!image) {
+        throw new NotFoundException();
+      }
+     /* if (!isAuthorized(user, image.idHouseImage )) { //image.idHouseImage 
+        throw new UnauthorizedException();
+      }*/
+      return await this.db.houseImages.update({
+        where: { idHouseImage: houseImageId },
+        data: { deleted: true },
+      });
+    } catch (error) {
+      handlePrismaError(error);
+    }
+  }
+
+
 }
